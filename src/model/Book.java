069e9e6fc710b340acefa78ae93f3005183f2409
duttaamit533-arch <duttamit533@gmail.com>
2026-03-28 16:14:57 +0530

    Added Book class with attributes and methods

---
+++ src/model/Book.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Book {
+
+    private String bookId;
+    private String title;
+    private String author;
+    private boolean isIssued;
+
+    public Book(String bookId, String title, String author) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.isIssued = false;
+    }
+
+    public String getBookId() { return bookId; }
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public boolean isIssued() { return isIssued; }
+
+    public void setIssued(boolean issued) {
+        this.isIssued = issued;
+    }
+
+    public void displayBook() {
+        System.out.println("ID: " + bookId +
+                ", Title: " + title +
+                ", Author: " + author +
+                ", Issued: " + isIssued);
+    }
+}
