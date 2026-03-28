117e1d7d1a4b7859d8858d6664ca65d3cb4d5d0f
duttaamit533-arch <duttamit533@gmail.com>
2026-03-28 16:21:12 +0530

    Added Main class to test Book functionality

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import model.Book;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Book b1 = new Book("101", "Java Basics", "James Gosling");
+
+        b1.displayBook();
+
+    }
+}
