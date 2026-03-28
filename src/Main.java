0715fc7beafb83a860e211f5b96a708e59ce44a7
duttaamit533-arch <duttamit533@gmail.com>
2026-03-28 16:28:36 +0530

    Connected LibraryService with Main for full functionality testing

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,32 @@
 import model.Book;
+import service.LibraryService;
 
 public class Main {
     public static void main(String[] args) {
 
-        Book b1 = new Book("101", "Java Basics", "James Gosling");
+        LibraryService library = new LibraryService();
 
-        b1.displayBook();
+        // Add books
+        library.addBook(new Book("101", "Java Basics", "James Gosling"));
+        library.addBook(new Book("102", "Data Structures", "Mark Allen"));
 
+        // Display books
+        System.out.println("\nAll Books:");
+        library.displayBooks();
+
+        // Issue book
+        System.out.println("\nIssuing Book 101:");
+        library.issueBook("101");
+
+        // Try issuing again (edge case)
+        library.issueBook("101");
+
+        // Return book
+        System.out.println("\nReturning Book 101:");
+        library.returnBook("101");
+
+        // Final display
+        System.out.println("\nFinal Book Status:");
+        library.displayBooks();
     }
 }
