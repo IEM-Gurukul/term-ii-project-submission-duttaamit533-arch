8d38f6c801fc0f565ff322a6d0b9adfd5e70fc35
duttaamit533-arch <duttamit533@gmail.com>
2026-03-28 16:43:04 +0530

    Updated Main to use ConsoleUI for interactive menu

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,10 @@
-import model.Book;
-import service.LibraryService;
+import ui.ConsoleUI;
 
 public class Main {
     public static void main(String[] args) {
 
-        LibraryService library = new LibraryService();
+        ConsoleUI ui = new ConsoleUI();
+        ui.start();
 
-        // Add books
-        library.addBook(new Book("101", "Java Basics", "James Gosling"));
-        library.addBook(new Book("102", "Data Structures", "Mark Allen"));
-
-        // Display books
-        System.out.println("\nAll Books:");
-        library.displayBooks();
-
-        // Issue book
-        System.out.println("\nIssuing Book 101:");
-        library.issueBook("101");
-
-        // Try issuing again (edge case)
-        library.issueBook("101");
-
-        // Return book
-        System.out.println("\nReturning Book 101:");
-        library.returnBook("101");
-
-        // Final display
-        System.out.println("\nFinal Book Status:");
-        library.displayBooks();
     }
 }
